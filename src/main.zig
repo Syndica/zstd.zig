c6b6c3c60aa582bcfd0e1d711e01b2dfb028ab5a
x19 <0x39015319@gmail.com>
2024-04-01 17:20:12 -0400

    expose module

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ pub usingnamespace @import("compress.zig");
 pub usingnamespace @import("decompress.zig");
 pub usingnamespace @import("types.zig");
 pub usingnamespace @import("error.zig");
+pub usingnamespace @import("reader.zig");
 
 const std = @import("std");
 const c = @import("c.zig");
